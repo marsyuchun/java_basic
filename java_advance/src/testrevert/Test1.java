a5aa61f1286caa631e2537992b1fc543a436c393
yuchun.yan <yuchun.yan@bianlifeng.com>
2020-05-15 15:23:45 +0800

    revert third

--- java_advance/src/testrevert/Test1.java
+++ java_advance/src/testrevert/Test1.java
@@ -6,7 +6,6 @@ package testrevert;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("第一次提交代码");
-        System.out.println("第二次提交代码");
     }
 
     void tostring(){

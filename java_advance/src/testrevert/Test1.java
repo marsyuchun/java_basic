4a20bf1151c69655109726d69b922471674c130a
yuchun.yan <yuchun.yan@bianlifeng.com>
2020-05-15 15:47:48 +0800

    recover

--- java_advance/src/testrevert/Test1.java
+++ java_advance/src/testrevert/Test1.java
@@ -6,6 +6,7 @@ package testrevert;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("第一次提交代码");
+        System.out.println("第二次提交代码");
     }
 
     void tostring(){
---
+++ java_advance/src/testrevert/Test3.java
@@ -0,0 +1,11 @@
+package testrevert;
+
+/**
+ * Created by yanyuchun on 2020/5/15
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("第二次提交");
+        System.out.println("第二次提交");
+    }
+}

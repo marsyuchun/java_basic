02f83b497277c19f14676a9dcdb26aee51daebec
yuchun.yan <yuchun.yan@bianlifeng.com>
2020-05-15 15:46:30 +0800

    add new

---
+++ java_advance/src/testrevert/Test5.java
@@ -0,0 +1,10 @@
+package testrevert;
+
+/**
+ * Created by yanyuchun on 2020/5/15
+ */
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println("revert 后再提交一次");
+    }
+}

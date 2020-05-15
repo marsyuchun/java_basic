9d276662eb33d1a173714ee71dc87a06c1640a8c
yuchun.yan <yuchun.yan@bianlifeng.com>
2020-05-15 15:19:43 +0800

    first

---
+++ java_advance/src/testrevert/Test1.java
@@ -0,0 +1,10 @@
+package testrevert;
+
+/**
+ * Created by yanyuchun on 2020/5/15
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("第一次提交代码");
+    }
+}
